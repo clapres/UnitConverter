53e653382b5fe9e85b6d8ec83f18deaf4448f95e
Claude Lapres <claude.lapres@gmail.com>
2022-02-23 22:29:47 -0500

    Init of input number

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let lenghtConversion = document.getElementById("length")
 let volumeConversion = document.getElementById("volume")
 let massConversion = document.getElementById("mass")
 
+document.getElementById("number").value = 0
+
 input.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
